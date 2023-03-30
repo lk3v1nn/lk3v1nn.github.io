bca7e7577702c9872bd29b449eca10596b622523
lk3v1nn <kevinlmorataya@gmail.com>
2023-03-30 11:35:12 -0600

    animacion de todo

--- funcionalidades/animaciones.js
+++ funcionalidades/animaciones.js
@@ -1,6 +1,14 @@
 function animEntrada(){
-    gsap.from('#tabla1, #tabla2',{
+
+    gsap.from('.areaParaArrastrar', {
     duration: 2.5,
+    y: '-100%',
+    ease: "bounce.out",
+    rotate: -90,
+});
+
+    gsap.from('#tabla1, #tabla2',{
+    duration: 3,
     x:  '-20vw',
     delay: 0,
     ease: "power4.out",
@@ -18,19 +26,21 @@ function animEntrada(){
     console.log('Animacion');
 
     gsap.to('#img1',{
-        yoyo: true,
-        duration: 2,
+        duration: 2.5,
         y:  '-50vw',
         ease: "bounce.out",
         rotate: 90,
         });
-        console.log('Animacion');
+
+    gsap.to('#img2',{
+        display: 'block',
+    });
     
     gsap.from('#img2',{
-    duration: 2,
-    y:  '-50vw',
-    ease: "bounce.out",
-    rotate: 90,
+        display: 'block',
+        duration: 2,
+        y:  '-50vw',
+        ease: "bounce.out",
+        rotate: 90,
     });
-    console.log('Animacion');
 }
