fa410355f239b98e9d44c4eef480c412684ddea9
lk3v1nn <kevinlmorataya@gmail.com>
2023-05-20 17:39:25 -0600

    f

--- funcionalidades/animProcesar.js
+++ funcionalidades/animProcesar.js
@@ -1,36 +1,36 @@
 
 function animEntrada(){
-    let tl = gsap.timeline({
-        repeat: 0,
-    });
+    // let tl = gsap.timeline({
+    //     repeat: 0,
+    // });
 
-    tl.from('.areaParaArrastrar', {
-        opacity: 0,    
-        duration: 5,
-        y: '-100%',
-        ease: "elastic",
-        rotate: -90,
-    });
+    // tl.from('.areaParaArrastrar', {
+    //     opacity: 0,    
+    //     duration: 5,
+    //     y: '-100%',
+    //     ease: "elastic",
+    //     rotate: -90,
+    // });
 
-    gsap.from('#tabla1, #tabla2',{
-    duration: 3,
-    x:  '-20vw',
-    delay: 0,
-    ease: "power4.out",
-    rotate: -25,
-    });
-    console.log('Animacion');
+    // gsap.from('#tabla1, #tabla2',{
+    // duration: 3,
+    // x:  '-20vw',
+    // delay: 0,
+    // ease: "power4.out",
+    // rotate: -25,
+    // });
+    // console.log('Animacion');
 
-    gsap.from('#tabla3',{
-    duration: 3,
-    x:  '20vw',
-    delay: 0,
-    ease: "power4.out",
-    rotate: 25,
-    });
-    console.log('Animacion');
+    // gsap.from('#tabla3',{
+    // duration: 3,
+    // x:  '20vw',
+    // delay: 0,
+    // ease: "power4.out",
+    // rotate: 25,
+    // });
+    // console.log('Animacion');
 
-    // gsap.to('#img1',{
+    // // gsap.to('#img1',{
     //     duration: 2.5,
     //     y:  '-50vw',
     //     ease: "bounce.out",
@@ -51,8 +51,8 @@ function animEntrada(){
 }
 
 function MostrarSecciones(){
-    document.querySelector('.seccionTablas').style.display = "flex";
-    document.querySelector('.seccionRecursividadIzquierda').style.display = 'flex';
+    document.querySelector('.seccionTablas').style.display = "inline-block";
+    document.querySelector('.seccionRecursividadIzquierda').style.display = 'inline-block';
 }
 
 function OcultarSecciones(){
