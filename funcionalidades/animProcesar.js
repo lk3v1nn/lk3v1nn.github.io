000699c409a242d8a23f91fb4f6f154f16d97097
lk3v1nn <kevinlmorataya@gmail.com>
2023-05-22 17:01:30 -0600

    diseño ete tu no lo tiene

--- funcionalidades/animProcesar.js
+++ funcionalidades/animProcesar.js
@@ -1,53 +1,34 @@
 
 function animEntrada(){
-    // let tl = gsap.timeline({
-    //     repeat: 0,
-    // });
+    let tl = gsap.timeline({
+        // duration: 2,
+        repeat: 0,
+    });
 
-    // tl.from('.areaParaArrastrar', {
-    //     opacity: 0,    
-    //     duration: 5,
-    //     y: '-100%',
-    //     ease: "elastic",
-    //     rotate: -90,
-    // });
+    tl.from('header', {
+        // opacity: 0,    
+        duration: 5,
+        // y: '-10%',
+        ease: "elastic",
+        rotate: -90,
+    });
 
-    // gsap.from('#tabla1, #tabla2',{
-    // duration: 3,
-    // x:  '-20vw',
-    // delay: 0,
-    // ease: "power4.out",
-    // rotate: -25,
-    // });
-    // console.log('Animacion');
+    gsap.from('.seccionBotones, .seccionTxt, .seccionRecursividadIzquierda', {
+        duration: 3,
+        x:  '-20vw',
+        delay: 0,
+        ease: "power4.out",
+        rotate: 25,
+    });
 
-    // gsap.from('#tabla3',{
-    // duration: 3,
-    // x:  '20vw',
-    // delay: 0,
-    // ease: "power4.out",
-    // rotate: 25,
-    // });
-    // console.log('Animacion');
-
-    // // gsap.to('#img1',{
-    //     duration: 2.5,
-    //     y:  '-50vw',
-    //     ease: "bounce.out",
-    //     rotate: 90,
-    //     });
-
-    // gsap.to('#img2',{
-    //     display: 'block',
-    // });
-    
-    // gsap.from('#img2',{
-    //     display: 'block',
-    //     duration: 2,
-    //     y:  '-50vw',
-    //     ease: "elastic",
-    //     rotate: 90,
-    // });
+    gsap.from('.seccionTablas',{
+    duration: 3,
+    x:  '-20vw',
+    delay: 0,
+    ease: "power4.out",
+    rotate: -25,
+    });
+   
 }
 
 function MostrarSecciones(){
